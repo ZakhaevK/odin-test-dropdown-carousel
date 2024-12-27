0ae2ffdeea50a44b17fb9925a1bf03131684c4bd
ZakhaevK <zkupke@gmail.com>
2024-12-27 14:34:33 +1100

    updated webpack files back to CommonJS

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,10 +4,11 @@ This is a Webpack configuration file that contains all details
 needed for bundling, like entry point, output destination, and 
 anything like plugins and loaders.
 */
-import { merge } from "webpack-merge";
-import common from "./webpack.common.js";
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-export default merge(common, {
+module.exports = merge(common, {
   mode: "production",
-  devtool: "source-map",
+  devtool: 'source-map',
 });
+
